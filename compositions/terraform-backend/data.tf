eb23269de023ced5994c6e2cdb16c29a2528e845
Sergi Barroso <hiroru@lionclan.org>
2023-07-07 11:18:58 +0200

    Removed unused data sources

--- compositions/terraform-backend/data.tf
+++ compositions/terraform-backend/data.tf
@@ -2,9 +2,3 @@ locals {
   country_short_name = split("-", var.region)[0] == "europe" ? "EU" : "US"
   region_short_name  = upper(split("-", var.region)[1])
 }
-
-data "google_client_config" "current" {}
-
-data "google_service_account" "object_viewer" {
-  account_id = "object-viewer"
-}
