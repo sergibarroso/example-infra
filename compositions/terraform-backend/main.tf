02ccadb0652d30367358f736d11896f6f346daa2
Sergi Barroso <hiroru@lionclan.org>
2023-07-07 11:33:20 +0200

    Fixed join functions

--- compositions/terraform-backend/main.tf
+++ compositions/terraform-backend/main.tf
@@ -2,7 +2,7 @@ module "terraform-backend" {
   source  = "terraform-google-modules/cloud-storage/google"
   version = "4.0.0"
 
-  name                     = join("-", var.project_id, "tf-backend")
+  names                    = [join("-", [var.project_id, "tf-backend"])]
   project_id               = var.project_id
   location                 = local.country_short_name
   public_access_prevention = true
@@ -10,10 +10,10 @@ module "terraform-backend" {
   admins                   = ["user:spam@example.com,eggs@example.com"]
 
   versioning = {
-    first = true
+    join("-", [var.project_id, "tf-backend"]) = true
   }
 
   custom_placement_config = {
-    data_locations : [join("-", local.country_short_name, local.region_short_name)]
+    data_locations : [join("-", [local.country_short_name, local.region_short_name])]
   }
 }
